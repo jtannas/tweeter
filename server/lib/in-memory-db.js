7579e7571937c7871a1675081ec5a8daee28c448
Joel Tannas <jtannas@gmail.com>
2018-01-29 14:16:07 -0700

    lint and format forked code

--- server/lib/in-memory-db.js
+++ server/lib/in-memory-db.js
@@ -2,9 +2,8 @@
 
 // Requiring a JSON file automatically parses it and returns the data. These
 // are just example tweets to make it less tedious to style the app initially.
-const db = {
-  tweets: require("../data-files/initial-tweets")
-}
+const tweets = require("../data-files/initial-tweets");
+
+const db = { tweets };
 
 module.exports = db;
-
