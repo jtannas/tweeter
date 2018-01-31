ada623592d2b9aac4bc38c4538bacc0b0d3066fc
Joel Tannas <jtannas@gmail.com>
2018-01-31 14:33:53 -0700

    async get tweets from server and render them

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -32,23 +32,6 @@ $(document).ready(function() {
     return 'Just now';
   };
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
-
   const createProfilePic = function createProfilePicElementFromTweetData(tweetData) {
     const $profilePic = $('<img>').addClass("profile-picture");
     $profilePic.attr({ src: tweetData.user.avatars.small, alt: 'profile picture' });
@@ -100,5 +83,12 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets([tweetData]);
+  const getTweets = function asyncGetTweetDataFromServer() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: tweets => renderTweets(tweets)
+    });
+  };
+  getTweets();
 });
