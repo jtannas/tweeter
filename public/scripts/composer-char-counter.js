938a3cf6644ff1e9194751825ab3b04e5ec4bce2
Joel Tannas <jtannas@gmail.com>
2018-01-31 17:38:02 -0700

    move the counter function def into the global namespace

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,10 @@
+const charCounter = function updateRemainingTweetLengthCounter(event) {
+  const currLen = $(this).val().length;
+  const counter = $(this).siblings('.counter');
+  counter.text(MAX_TWEET_LENGTH - currLen);
+  counter.toggleClass('invalid', currLen > MAX_TWEET_LENGTH);
+};
+
 $(document).ready(() => {
-  const MAX_TWEET_LEN = 140;
-
-  const charCounter = function updateRemainingTweetLengthCounter(event) {
-    const currLen = $(this).val().length;
-    const counter = $(this).siblings('.counter');
-    counter.text(MAX_TWEET_LEN - currLen);
-    counter.toggleClass('invalid', currLen > MAX_TWEET_LEN);
-  };
-
   $('.new-tweet').on('input', 'textarea', charCounter);
 });
