20833d8fd50808148220490eeb991d768387441d
Joel Tannas <jtannas@gmail.com>
2018-02-01 16:11:56 -0700

    refactor the tweets routes into logical sections

--- server/index.js
+++ server/index.js
@@ -1,32 +1,48 @@
 "use strict";
 
-const PORT = 8080;
+/** Imports */
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+const { MongoClient } = require("mongodb");
 
 const dataHelpersFactory = require("./lib/data-helpers.js");
 const tweetsRoutesFactory = require("./routes/tweets");
 
-const { MongoClient } = require("mongodb");
+
+/** Settings */
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+const APP_PORT = 8080;
 
-MongoClient.connect(MONGODB_URI, (err, db) => {
-  if (err) {
-    console.error(`Failed to connect: ${MONGODB_URI}`);
-    throw err;
-  }
 
-  console.log(`Connected to mongodb: ${MONGODB_URI}`);
-  const DataHelpers = dataHelpersFactory(db);
-  const tweetsRoutes = tweetsRoutesFactory(DataHelpers);
+/** App Factory Function*/
+const createApp = function createAndConfigWebServer(path, routes, staticDir = "public") {
+  const app = express();
 
-  app.use("/tweets", tweetsRoutes);
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static(staticDir));
+  app.use(path, routes);
 
-  app.listen(PORT, () => {
-    console.log("Example app listening on port " + PORT);
+  return app;
+};
+
+/** Connection Factory Function - */
+const createConnection = function createAndConfigDbConnection(callback) {
+  MongoClient.connect(MONGODB_URI, (err, db) => {
+    if (err) {
+      console.error(`Failed to connect: ${MONGODB_URI}`);
+      throw err;
+    }
+    console.log(`Connected to mongodb: ${MONGODB_URI}`);
+    callback(db);
+  });
+};
+
+
+/** Execute */
+createConnection((db) => {
+  const dataHelpers = dataHelpersFactory(db);
+  const tweetsRoutes = tweetsRoutesFactory(dataHelpers);
+  createApp("/tweets", tweetsRoutes).listen(APP_PORT, () => {
+    console.log("Tweeter listening on port " + APP_PORT);
   });
 });
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,44 +1,58 @@
 "use strict";
 
+/** Imports */
+const express = require('express');
 const userHelper = require("../lib/util/user-helper");
 
-const express = require('express');
-const tweetsRoutes = express.Router();
+/** Helper Functions */
+const errHandler = function internalServerErrorHandler(err, req, res) {
+  if (err) {
+    res.status(500).json({ error: err.message });
+    return true;
+  }
+  return false;
+};
 
+const tweetInvalidHandler = function tweetInvalidErrorHandler(req, res) {
+  const validationErrors = [];
+  if (!req.body.text) { validationErrors.push('invalid request: no data in POST body'); }
+
+  if (validationErrors.length) {
+    res.status(400).json({ errors: validationErrors });
+    return true;
+  }
+  return false;
+};
+
+const parseTweet = function parseReqBodyIntoTweetDocument(req) {
+  const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+  return {
+    user: user,
+    content: {
+      text: req.body.text
+    },
+    createdAt: Date.now()
+  };
+};
+
+
+/** Route Factory */
 module.exports = function(DataHelpers) {
+
+  const tweetsRoutes = express.Router();
 
   tweetsRoutes.get("/", function(req, res) {
     DataHelpers.getTweets((err, tweets) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.json(tweets);
-      }
+      err ? errHandler(err, req, res) : res.json(tweets);
     });
   });
 
   tweetsRoutes.post("/", function(req, res) {
-    if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body' });
-      return;
+    if (!tweetInvalidHandler(req, res)) {
+      DataHelpers.saveTweet(parseTweet(req), (err) => {
+        err ? errHandler(err, req, res) : res.status(201).send();
+      });
     }
-
-    const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
-    const tweet = {
-      user: user,
-      content: {
-        text: req.body.text
-      },
-      createdAt: Date.now()
-    };
-
-    DataHelpers.saveTweet(tweet, (err) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.status(201).send();
-      }
-    });
   });
 
   return tweetsRoutes;
