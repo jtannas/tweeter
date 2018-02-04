25cf4c060870d8e20abe76eb200364d61711e60d
Joel Tannas <jtannas@gmail.com>
2018-02-03 22:34:09 -0700

    add a util for inspecting document cookies

---
+++ public/scripts/utils/cookies.js
@@ -0,0 +1,21 @@
+function getCookie(name) {
+  // https://stackoverflow.com/a/5968306/7950458
+  let dc = document.cookie;
+
+  let prefix = name + "=";
+  let begin = dc.indexOf("; " + prefix);
+  let end;
+  if (begin === -1) {
+    begin = dc.indexOf(prefix);
+    if (begin !== 0) { return null; }
+  } else {
+    begin += 2;
+    end = document.cookie.indexOf(";", begin);
+    if (end === -1) {
+      end = dc.length;
+    }
+  }
+  // because unescape has been deprecated, replaced with decodeURI
+  //return unescape(dc.substring(begin + prefix.length, end));
+  return decodeURI(dc.substring(begin + prefix.length, end));
+}
